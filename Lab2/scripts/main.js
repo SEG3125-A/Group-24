7efedb8cd6c6793f8b920c9a6f9a8166c1db7ad0
Amani <alouendr@uottawa.ca>
2024-01-26 12:04:07 -0500

    added customer form, filtered products and added cart section

--- Lab2/scripts/main.js
+++ Lab2/scripts/main.js
@@ -8,18 +8,11 @@ async function getData(file) {
     let items = parsedText.products;
     return items;
 }
+// items is an array of objects
 
 // criteria to sort
 function comparePrice(a,b) {
     return (a.price - b.price);
-}
-
-const show_products = (items) => {
-    let menu = document.getElementsByClassName("menu");
-    menu[0].style.display = "block";
-    let products = document.getElementsByClassName("products");
-    products.innerHTML = "";
-    construct_list_goods(items,products,0);
 }
 
 const construct_list_goods = (items,products,flag) => {
@@ -36,8 +29,10 @@ const construct_list_goods = (items,products,flag) => {
             // <h3 class="type_diet">Halal</h3>
             // <h3 class="org">non organic</h3>
         // </div>
+        //<div class="input_ck">
         // <input type="checkbox"></input>
         //<label></label>
+        //</div>
     // </div>
 
     for (i = 0;i<items.length;i++) {
@@ -86,29 +81,32 @@ const construct_list_goods = (items,products,flag) => {
         // if flag == 1 then we add a checkbox to add to the cart
 
         if (flag) {
+            var div_input = document.createElement("div");
+            div_input.className = "divInput";
+            div.appendChild(div_input);
             let checkbox = document.createElement("input");
             checkbox.type = "checkbox";
             checkbox.id = i;
             checkbox.value = product.name;
             checkbox.dataset.object = JSON.stringify(product);
             checkbox.name = "product";
-            div.appendChild(checkbox);
+            div_input.appendChild(checkbox);
 
             let label = document.createElement("label");
             label.htmlFor = i;
             label.appendChild(document.createTextNode("Add to cart"));
-            div.appendChild(label);   
+            div_input.appendChild(label);  
+
         }
     }
+}
 
-    if (flag) {
-        let btn = document.createElement("button");
-        btn.onclick = function() {add_to_cart()};
-        btn.appendChild(document.createTextNode("Pay now"));
-        products[0].appendChild(btn);
-    }
-
-
+const show_products = (items) => {
+    let menu = document.getElementsByClassName("menu");
+    menu[0].style.display = "block";
+    let products = document.getElementsByClassName("products");
+    products.innerHTML = "";
+    construct_list_goods(items,products,0);
 }
 
 async function main() {
@@ -117,6 +115,54 @@ async function main() {
     items.sort(comparePrice);
 
     show_products(items);
+}
+
+// open section is a function both called when clicking on a tab, or by the parent function go_to_customer_form()
+const open_section = (evt,id) => {
+    let tabs = ["client","products","cart"];
+    // getting the sections that hold actual information 
+    client = document.getElementById("customer");
+    products = document.getElementById("goods_hero");
+    cart = document.getElementById("Pay");
+
+    let new_id = "";
+
+    // based on the tab id received, I can see which section I want to show.
+    if (id == "client") {
+        new_id = "customer";
+    } else if (id == "products") {
+        new_id = "goods_hero";
+    } else if (id == "cart") {
+        new_id = "Pay";
+    }
+
+    // to be sure, hiding all sections
+
+    client.style.display = "none";
+    products.style.display = "none";
+    cart.style.display = "none";
+
+    // removing active classname on all tablinks
+    tablinks = document.getElementsByClassName("tablinks");
+	for (i = 0; i < tablinks.length; i++) {
+		tablinks[i].className = tablinks[i].className.replace(" active", "");
+	}
+
+    // display the appropriate section
+    document.getElementById(new_id).style.display = "block";  
+
+    // enabling all tabs that are previous the current tab I am in.
+
+    for (i = 0;i<tabs.indexOf(id);i++){
+        document.getElementById(tabs[i]).disabled = false;
+    }
+    // disabling the button id where I am.
+    document.getElementById(id).disabled = true;
+
+    // adding an active className on the current one
+    if (evt != null) {
+        evt.currentTarget.className += " active";
+    }
 }
 
 main();
